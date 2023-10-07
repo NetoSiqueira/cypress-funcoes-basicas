30f51896295c0ed9d46afc361caad2fc09317679
Siqueira Neto <siqueiranetoufc@gmail.com>
2023-10-07 18:01:27 -0300

    Pegando elementos do tipo Link na tela

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -2,13 +2,25 @@
 
 describe('Work with basic elements', () =>{
 
+   
     it('Text', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
-
         cy.get('.facilAchar').should('contain', 'Cuidado onde clica, muitas armadilhas...')
         cy.get('span').should('contain', 'Cuidado onde clica, muitas armadilhas...')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     });
 
+    it.only('Links', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+        cy.reload()
+
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+    });
 
 })
