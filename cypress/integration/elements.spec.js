056baee1be51120dfdd7cc847a9314a116a6c6a9
Siqueira Neto <siqueiranetoufc@gmail.com>
2023-10-08 15:03:21 -0300

    Pegando elementos do tipo radio na tela

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -51,4 +51,15 @@ describe('Work with basic elements', () =>{
 
     })
 
+    it.only('RadioButton', () => {
+        cy.get('#formSexoFem')
+        .click()
+        .should('be.checked')
+
+        cy.get('#formSexoMasc')
+        .should('not.be.checked')
+
+        cy.get("[name='formSexo']").should('have.length', 2)
+    });
+
 })
