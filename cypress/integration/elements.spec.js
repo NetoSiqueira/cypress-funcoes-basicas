54b9fe167d8dc805ad060119a75b142aa2af7ad9
Siqueira Neto <siqueiranetoufc@gmail.com>
2023-10-08 14:54:53 -0300

    Pegando elementos do tipo texto na tela

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -2,16 +2,22 @@
 
 describe('Work with basic elements', () =>{
 
+    before(() =>{
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() =>{
+        cy.reload()
+    })
+
    
     it('Text', () => {
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
         cy.get('.facilAchar').should('contain', 'Cuidado onde clica, muitas armadilhas...')
         cy.get('span').should('contain', 'Cuidado onde clica, muitas armadilhas...')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     });
 
-    it.only('Links', () => {
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    it('Links', () => {
         cy.get('[href="#"]').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
 
@@ -22,5 +28,27 @@ describe('Work with basic elements', () =>{
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
     });
+
+    it.only('TextFields', () => {
+        cy.get('#formNome').type('Cypress test')
+        cy.get('#formNome').should('have.value', 'Cypress test')
+      
+        cy.get('#elementosForm\\:sugestoes')
+        .type('textarea')
+        .should('have.value', 'textarea')
+        
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+        .type('???')
+       
+        cy.get('[data-cy=dataSobrenome]')
+        .type('teste123{backspace}')
+        .should('have.value', 'teste12')
+
+        cy.get('#elementosForm\\:sugestoes')
+        .clear()
+        .type('Erro{selectall}acerto', {delay:100})
+        .should('have.value', 'acerto')
+
+    })
 
 })
