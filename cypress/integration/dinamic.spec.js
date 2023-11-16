e71c59ab6ce87045d95404c7bcb164145a1d54db
Siqueira Neto <siqueiranetoufc@gmail.com>
2023-11-16 09:00:59 -0300

    Criaçcao teste dinamico com each

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -26,4 +26,22 @@ describe('Dinamic test', () =>{
         cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
         });
     })
+
+    it.only('Deve selecionar todo usando o each', () => {
+        cy.get('#formNome').type('Usuario')
+        cy.get('#formSobrenome').type('Qualquer')
+        cy.get(`[name=formSexo][value=F]`).click()
+
+       // cy.get('[name=formComidaFavorita]').click({multiple: true})
+       cy.get('[name=formComidaFavorita]').each($el =>{
+            if($el.val() != 'vegetariano')
+                cy.wrap($el).click()
+       })
+
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+        //cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
+    });
 })
